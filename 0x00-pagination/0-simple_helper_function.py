25ac0368992d72317dbfadc92e9c07a11e92f507
Flyer <115190839+MrSakine@users.noreply.github.com>
2024-05-16 12:13:22 +0000

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,7 +5,7 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    eturn a tuple of size two containing a start index
+    Return a tuple of size two containing a start index
     and an end index corresponding to the range of indexes
     to return in a list for those particular pagination parameters
     """
