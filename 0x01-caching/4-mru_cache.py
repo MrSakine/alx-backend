4ede932d9ed8569d852518de50fd8659320db9e6
Flyer <115190839+MrSakine@users.noreply.github.com>
2024-05-21 16:39:45 +0000

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ MRUCache Caching """
 BaseCaching = __import__("base_caching").BaseCaching
+from collections import OrderedDict
 
 
 class MRUCache(BaseCaching):
@@ -9,31 +10,28 @@ class MRUCache(BaseCaching):
     """
     def __init__(self):
         super().__init__()
-        self.recent_keys = []
+        self.cache_data = OrderedDict()
 
     def get(self, key):
         """
         Return the value in self.cache_data linked to key
         """
-        if key in self.cache_data:
-            self.recent_keys.remove(key)
-            self.recent_keys.append(key)
-            return self.cache_data[key]
-        else:
-            return None
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
 
     def put(self, key, item):
         """
         Assign to the dictionary self.cache_data,
         the item value for the key
         """
-        if key in self.cache_data:
-            self.cache_data[key] = item
-            self.recent_keys.remove(key)
-        else:
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                mru_key = self.recent_keys.pop()
-                del self.cache_data[mru_key]
+                mru_key = self.cache_data.popitem(False)[0]
                 print("DISCARD: {}".format(mru_key))
             self.cache_data[key] = item
-        self.recent_keys.append(key)
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
