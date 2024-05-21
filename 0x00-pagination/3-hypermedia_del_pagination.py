a2283bd2320fec1a1b075800388d7bb93f5b77c8
Flyer <115190839+MrSakine@users.noreply.github.com>
2024-05-21 10:37:16 +0000

    Update 3-hypermedia_del_pagination.py

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -35,11 +35,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(
-        self,
-        index: int = None,
-        page_size: int = 10
-    ) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10):
         """
         Returns a dictionary with the following key-value pairs:
         - index: the current start index of the return page.
