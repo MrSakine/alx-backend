a5214b960639411fdee3a4910cbad97c839e97e0
Flyer <115190839+MrSakine@users.noreply.github.com>
2024-05-21 15:44:58 +0000

    Create node.py

---
+++ 0x01-caching/node.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Node class """
+class Node:
+    """ Node class definition """
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+        
+    def __str(self):
+        """String representation"""
+        return self.value
