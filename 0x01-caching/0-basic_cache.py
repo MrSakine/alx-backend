a10d014909d0c729d04cc4ed58b8f915f3fd9220
MrSakine <msakine20@gmail.com>
2024-05-21 09:38:35 +0000

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Basic dictionary """
+BaseCaching = __import__('main').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache class definition
+    """
+    def __init__(self) -> None:
+        super().__init__()
+
+    def put(self, key, item) -> None:
+        """
+        Set a new item into the cache through @key
+        """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data.update({key: item})
+
+    def get(self, key):
+        """
+        Return the value in self.cache_data linked to key
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key)
