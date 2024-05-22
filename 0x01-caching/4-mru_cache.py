c1a2f1837a117ce2d9bfa87dd7d476fff9eb77df
Flyer <115190839+MrSakine@users.noreply.github.com>
2024-05-22 14:17:02 +0000

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ MRUCache Caching """
-from collections import OrderedDict
 BaseCaching = __import__("base_caching").BaseCaching
 
 
@@ -8,30 +7,39 @@ class MRUCache(BaseCaching):
     """
     MRUCache class definition
     """
-    def __init__(self):
+
+    def __init__(self) -> None:
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.__count = 0
+        self.__keys = []
+        self.__counts = []
+
+    def put(self, key, item) -> None:
+        """
+        Assign to the dictionary self.cache_data,
+        the item value for the key
+        """
+        if not key or not item:
+            return
+        if key in self.__keys:
+            index = self.__keys.index(key)
+            self.__keys.pop(index)
+            self.__counts.pop(index)
+        if len(self.__keys) == BaseCaching.MAX_ITEMS:
+            index = self.__counts.index(max(self.__counts))
+            self.__counts.pop(index)
+            self.cache_data.pop(self.__keys[index])
+            print('DISCARD: {}'.format(self.__keys.pop(index)))
+        self.__keys.append(key)
+        self.__counts.append(self.__count)
+        self.cache_data.update({key: item})
+        self.__count += 1
 
     def get(self, key):
         """
         Return the value in self.cache_data linked to key
         """
-        if key is not None and key in self.cache_data:
-            self.cache_data.move_to_end(key, last=False)
-        return self.cache_data.get(key, None)
-
-    def put(self, key, item):
-        """
-        Assign to the dictionary self.cache_data,
-        the item value for the key
-        """
-        if key is None or item is None:
-            return
-        if key not in self.cache_data:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                mru_key = self.cache_data.popitem(False)[0]
-                print("DISCARD: {}".format(mru_key))
-            self.cache_data[key] = item
-            self.cache_data.move_to_end(key, last=False)
-        else:
-            self.cache_data[key] = item
+        if key in self.__keys:
+            self.__count += 1
+            self.__counts[self.__keys.index(key)] = self.__count
+        return self.cache_data.get(key)
