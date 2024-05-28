d0bcfe41a66a9a82350c2770cc0fc622b2115fcf
MrSakine <msakine20@gmail.com>
2024-05-28 14:27:31 +0000

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Basic Babel setup """
+from config import Config
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+babel = Babel(
+    default_locale=Config.LANGUAGES[0],
+    default_timezone=Config.TIMEZONES[0]
+)
+
+
+@app.route("/")
+def home():
+    """ Display content from html files for the route """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    babel.init_app(app)
+    app.run(host='0.0.0.0', port=5001)
---
+++ 0x02-i18n/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Babel config """
+
+
+class Config:
+    """ Config class definition """
+    LANGUAGES = ["en", "fr"]
+    TIMEZONES = ["UTC"]
