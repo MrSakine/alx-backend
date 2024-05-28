e572be635b4798144143b3e0ae48b4f02c3e74b4
Flyer <115190839+MrSakine@users.noreply.github.com>
2024-05-28 15:11:31 +0000

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,15 +3,13 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-# app.url_map.strict_slashes = False
 
 
 @app.route("/")
-def home():
+def index():
     """ Display content from html files for the route """
     return render_template("0-index.html")
 
 
 if __name__ == "__main__":
-    """ Main Function """
     app.run(debug=True)
